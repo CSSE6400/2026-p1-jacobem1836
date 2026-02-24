b8cd6ddf907eb640a92bb1a800f8f416ef5d9602
Jacob Marriott <jacobemarriott@icloud.com>
2026-02-25 08:53:14 +1000

    Add get functionality

--- views/routes.py
+++ views/routes.py
@@ -5,3 +5,8 @@ api = Blueprint('api', __name__, url_prefix='/api/v1')
 @api.route('/health')
 def health():
     return jsonify({"status": "ok"})
+
+@api.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify([])
+
